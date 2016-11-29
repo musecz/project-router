cffcfd06a0b2a005acb48651b821722cf4bb49e4
musecz <musecz@wanadoo.fr>
2016-11-29 20:57:58 +0100

    Feature Complete
    Creation of two methods :
    *getContacts : Fetch the list of Contact
    
    *getContact : Fetch a Contact by Id

--- src/app/contact/contact.service.ts
+++ src/app/contact/contact.service.ts
@@ -1,8 +1,45 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Observable} from "rxjs";
+
+export class Contact {
+    constructor(public id: number, public name: string, public lastname: string) {
+    }
+}
+
+const CONTACT: Array<Contact> = [
+    new Contact(1, 'Tonton', 'Flingeur'),
+    new Contact(1, 'John', 'Doe'),
+    new Contact(1, 'Steven', 'Seagal'),
+    new Contact(1, 'Elvis', 'Bert')
+];
 
 @Injectable()
 export class ContactService {
+    public contacts: Array<Contact> = CONTACT;
+    /**
+     * Fetch the list of Contact - Simulate a timeout of 3 seconds.
+     * @returns {Observable<Array<Contact>>}
+     */
+    getContacts(): Observable<Array<Contact>> {
+        return Observable.create((observer) => {
+            setTimeout(() => {
+                observer.next(CONTACT);
+                observer.complete();
+            }, 3000);
+        });
+    }
 
-  constructor() { }
+    /**
+     * Fetch a Contact by id
+     * @param id
+     * @returns {Observable<Contact>}
+     */
+    getContact(id: number): Observable<Contact> {
+        return Observable.create((observer) => {
+            setTimeout(() => {
+                observer.next(this.contacts.find(contact => contact.id === id ))
+            },3000)
+        })
+    }
 
 }
