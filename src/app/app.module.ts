92b72724e97e9b64a24dd32f03e7051ca447847d
musecz <musecz@wanadoo.fr>
2016-12-01 11:04:40 +0100

    moving the app-routing module
    -When two modules are in the same path, angular-cli generate an error  Multiple module files found:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './shared/app-routing.module';
 
 
 
